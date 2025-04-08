7cd6b78c72c092c368371b7986fb777fcb4f7722
alejandroreyes27 <alejandroariza.cr@gmail.com>
2025-04-07 19:11:26 -0500

    Cargar archivos de mi proyecto

---
+++ app/__init__.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
+
+# Instanciamos las extensiones
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.urandom(24)
+    app.config.from_object('config.Config')  # Cargar configuración desde config.py
+
+    # Inicializar las extensiones
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    @login_manager.user_loader
+    def load_user(idUser):
+        from app.models.users import Users
+        return Users.query.get(int(idUser))
+
+    # Registrar blueprints y cargar modelos
+    with app.app_context():
+        # Importar modelos para que Flask-Migrate los detecte
+        from app.models.Categoria import Categoria
+        from app.models.Productos import Productos
+
+        # Importar y registrar blueprints
+        from app.routes.auth import bp as auth_bp
+        from app.routes.Productos_routes import bp as productos_bp
+        from app.routes.Carrito_routes import bp as carrito_bp
+        from app.routes.Categoria_routes import bp as categoria_bp 
+
+        app.register_blueprint(auth_bp)
+        app.register_blueprint(productos_bp)
+        app.register_blueprint(carrito_bp)
+        app.register_blueprint(categoria_bp)
+    return app
